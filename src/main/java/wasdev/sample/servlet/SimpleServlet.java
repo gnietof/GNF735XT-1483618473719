d9d2a917fc5b53360d95ba700a189c61c5b69940
Genaro Nieto Fernandez <genaro@es.ibm.com>
2017-01-05 06:24:32 -0600

    Cambios iniciales de prueba.

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class SimpleServlet
  */
-@WebServlet("/SimpleServlet")
+@WebServlet("/Test")
 public class SimpleServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
@@ -20,8 +20,8 @@ public class SimpleServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        response.getWriter().print("Hello World!");
+        response.setContentType("application/json");
+        response.getWriter().print("{'name':'Genaro Nieto'}");
     }
 
 }
