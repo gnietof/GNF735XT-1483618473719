23fbb29add5c2e5f5492cb7c78f5f45d36fa10ca
Genaro Nieto Fernandez <genaro@es.ibm.com>
2017-02-08 13:23:33 -0600

    Donde dije input digo output

--- src/main/java/wasdev/sample/servlet/ListenerServlet.java
+++ src/main/java/wasdev/sample/servlet/ListenerServlet.java
@@ -82,8 +82,8 @@ public class ListenerServlet extends HttpServlet {
 
 		URL url = new URL("http://vzzmqi.messaging.internetofthings.ibmcloud.com:1883/api/v0002/device/types/Garmin/devices/gnf735xt/events/HR");
 		HttpURLConnection uc = (HttpURLConnection)url.openConnection();
-		uc.setDoInput(true);
-		uc.setDoOutput(false);
+		uc.setDoInput(false);
+		uc.setDoOutput(true);
 		uc.setRequestMethod("POST");
 //		uc.setRequestProperty("Accept","application/json");
 		uc.setRequestProperty("Content-Type","application/json");
