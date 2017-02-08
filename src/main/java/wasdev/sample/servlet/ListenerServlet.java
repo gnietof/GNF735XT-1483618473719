d3c01b728f1a7d11b6e435ebccc5b756fb5a3067
Genaro Nieto Fernandez <genaro@es.ibm.com>
2017-02-08 07:04:45 -0600

    Primeros pasos pasarela Garmin -> IoT

--- src/main/java/wasdev/sample/servlet/ListenerServlet.java
+++ src/main/java/wasdev/sample/servlet/ListenerServlet.java
@@ -1,14 +1,23 @@
 package wasdev.sample.servlet;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import redis.clients.jedis.Jedis;
+//import redis.clients.jedis.Jedis;
 import org.cloudfoundry.runtime.env.CloudEnvironment;
+
+import com.ibm.json.java.JSONObject;
+import com.ibm.misc.BASE64Encoder;
 
 /**
  * Servlet implementation class SimpleServlet
@@ -16,6 +25,8 @@ import org.cloudfoundry.runtime.env.CloudEnvironment;
 @WebServlet("/Listen")
 public class ListenerServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+	private static final String USER = "use-token-auth";
+	private static final String SECRET = "gnf735XT";
 
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -23,10 +34,22 @@ public class ListenerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	
+/*
+		BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
+		String s="";
+		while ((s = br.readLine()) != null) {
+			System.out.println(s);
+		}
+		br.close();		
+*/		
+		JSONObject ji = new JSONObject();
+		ji.parse(request.getInputStream());
+		
+/*
     	String hr = request.getParameter("hr");
     	System.out.println(hr);
     	
-    	String lat = request.getParameter("lat");
+ 		String lat = request.getParameter("lat");
     	System.out.println(lat);
     	
     	String lng = request.getParameter("lng");
@@ -34,12 +57,27 @@ public class ListenerServlet extends HttpServlet {
     	
     	String spd = request.getParameter("spd");
     	System.out.println(spd);
-    	
-		Object[] serviceInfo = getServiceInfo();
-      	Jedis jedis = new Jedis((String)serviceInfo[0], (Integer)serviceInfo[1]);
-      	
+ */   	
+//		Object[] serviceInfo = getServiceInfo();
+
         response.setContentType("application/json");
         response.getWriter().print("{\"rc\":\"200\"}");
+
+		URL url = new URL("http://vzzmqi.messaging.internetofthings.ibmcloud.com:1883/api/v0002/device/types/Garmin/devices/gnf735xt/events/HR");
+		HttpURLConnection uc = (HttpURLConnection)url.openConnection();
+		uc.setDoInput(true);
+		uc.setDoOutput(true);
+		uc.setRequestMethod(method);
+		uc.setRequestProperty("Accept","application/json");
+		uc.setRequestProperty("Content-Type","application/json");
+		BASE64Encoder b64 = new BASE64Encoder();
+		String encoded = b64.encode((USER + ":" + SECRET).getBytes());
+		uc.setRequestProperty("Authorization", "Basic " + encoded);
+		OutputStream os = uc.getOutputStream();
+		os.write(ji.toString().getBytes());
+		os.close();
+
+      	
     }
     
     
